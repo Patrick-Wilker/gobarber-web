dcb2173c1529be0906828d49e548fb759f119a07
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-13 21:41:05 -0300

    Configure private routes

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Dashboard(){
-    return <div />
+    return (
+        <h1>Dashboard</h1>
+    );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function SignIn(){
-    return <div />
+    return (
+        <h1>SignIn</h1>
+    );
 }
